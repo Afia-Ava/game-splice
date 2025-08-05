98ecd1568d08057edf778c2bd17688c569a6b3b7
Afia Ava <afiakhanomava@gmail.com>
2025-08-04 21:06:39 -0400

    sign up fix

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCisHdetHnaIywC3x_q4QtvQ1N0uOHYONk',
+  authDomain: 'game-splice.firebaseapp.com',
+  projectId: 'game-splice',
+  storageBucket: 'game-splice.appspot.com',
+  messagingSenderId: '206158975406',
+  appId: '1:206158975406:web:9a635491c8c043f554783f',
+};
+
+window.firebaseConfig = firebaseConfig;
